bc7eac9ce5c8e24b34349c7fba8d30b6b9d4cc1a
YC <jason.lee.81306@gmail.com>
2023-02-07 01:03:47 +0800

    TMP import , Scene Update

---
+++ Guitar_Teaching/Assets/Script/UI/ButtonChangeScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonChangeScene : MonoBehaviour
+{
+    #region function
+
+    public void GoToScene(string NewSceneName)
+    {
+
+    }
+
+    #endregion
+}
