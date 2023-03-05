7a1943a828dae2e0bb569494733252a0e5ebe9f3
YC <jason.lee.81306@gmail.com>
2023-03-05 17:25:38 +0800

    AR相機鏡射完成

---
+++ Guitar_Teaching/Assets/Script/Cam/CamMirror.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Mirror the attached Camera 
+/// </summary>
+public class CamMirror : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera cam = GetComponent<Camera>();
+        if(cam != null)
+		{
+            Debug.Log("Have Cam to Mirror");
+            MirrorFlipCamera(cam);
+        }
+    }
+
+	#region function
+
+    void MirrorFlipCamera(Camera cam)
+	{
+        Matrix4x4 CamProjectMat = cam.projectionMatrix;
+        CamProjectMat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        cam.projectionMatrix = CamProjectMat;
+	}
+
+	#endregion
+}
